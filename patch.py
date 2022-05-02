a51d7466178096414dc568a6e1a7dd913a2ee4bd
rekaerst <rekaerst@outlook.com>
2022-05-03 00:04:37 +0800

    formatting

--- patch.py
+++ patch.py
@@ -6,12 +6,16 @@ import sys
 # c3                ret
 CODE = bytes.fromhex('b8 01 00 00 00 c3')
 
+
 def main():
     if len(sys.argv) != 2:
         print(f"Usage {sys.argv[0]} <path of libmkl_core.so>")
         return 1
     try:
-        output = subprocess.check_output(f'objdump -T {sys.argv[1]} | grep mkl_serv_intel_cpu',shell=True, text=True).rstrip('\n').split('\n')
+        output = subprocess.check_output(
+            f'objdump -T {sys.argv[1]} | grep mkl_serv_intel_cpu',
+            shell=True,
+            text=True).rstrip('\n').split('\n')
         offsets = [int(i.split(' ')[0], 16) for i in output]
 
     except subprocess.CalledProcessError:
@@ -23,9 +27,10 @@ def main():
             for offset in offsets:
                 file.seek(offset, 0)
                 file.write(CODE)
-        print("patch successed")
-    except:
-        print("cannot write to file, do you have write permission to the file?")
+        print("patch succeed")
+    except PermissionError:
+        print(
+            "cannot write to file, do you have write permission to the file?")
 
     return 0
 
